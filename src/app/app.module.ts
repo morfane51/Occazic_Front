608b2ed74c5807ac03d4c08fd0d9b7a7cb3edb88
aymeric <aymeric.baptiste@gmail.com>
2021-12-15 23:59:31 +0100

    Method stable for input form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatSelectModule} from "@angular/material/select";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
@@ -23,19 +23,20 @@ import {MatIconModule} from "@angular/material/icon";
     EstimateComponent,
     NavBarComponent,
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatStepperModule,
-    MatInputModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    MatSelectModule,
-    MatToolbarModule,
-    MatIconModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatSliderModule,
+        MatStepperModule,
+        MatInputModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatToolbarModule,
+        MatIconModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/estimate/estimate.component.ts
+++ src/app/estimate/estimate.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators, FormControl, FormArray} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 interface Cat {
   _id: string;
   name: string;
+}
+
+interface Input_temp {
+  _id: string;
+  name: string;
+  array: boolean;
+  category: string;
 }
 
 interface Input {
@@ -14,10 +21,19 @@ interface Input {
   category: string;
 }
 
+interface Input_array_temp {
+  _id: string
+  name: string;
+  val_func_id: string;
+}
+
 interface Input_array {
   _id: string
   name: string;
   val_func_id: string;
+}
+
+interface Post_lists {
 }
 
 @Component({
@@ -28,17 +44,23 @@ interface Input_array {
 
 
 export class EstimateComponent implements OnInit {
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  threeFormGroup: FormGroup;
-  isEditable = true;
+
+  productFormGroup: FormGroup;
+  inputFormGroup: FormGroup;
+  donneeFormGroup: FormGroup;
+
+  isEditable = false;
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
   catList: Cat[] = [];
-  inputLists: Input[] = [];
-  input_array_Lists: Input_array[] = [];
 
-  cat_selected = '61901df3d458e1c870b345c9';
+  inputLists: Input[] = [];
+  inputLists_temp: Input_temp[] = [];
+
+  input_array_Lists: Input_array[] = [];
+  input_array_Lists_temp: Input_array_temp[] = [];
+
+  post_Lists: Post_lists[] = [];
 
   private _catListUrl = 'http://localhost:3000/category';
   private _inputListUrl = 'http://localhost:3000/val_func';
@@ -51,22 +73,103 @@ export class EstimateComponent implements OnInit {
     this._httpClient.get<Cat[]>(this._catListUrl).subscribe(data => {
       this.catList = data;
     })
-    this._httpClient.get<Input[]>(this._inputListUrl).subscribe(data => {
-      this.inputLists = data;
+    this._httpClient.get<Input_temp[]>(this._inputListUrl).subscribe(data => {
+      this.inputLists_temp = data;
     })
-    this._httpClient.get<Input_array[]>(this._input_array_ListUrl).subscribe(data => {
-      this.input_array_Lists = data;
+    this._httpClient.get<Input_array_temp[]>(this._input_array_ListUrl).subscribe(data => {
+      this.input_array_Lists_temp = data;
     })
 
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
+    this.productFormGroup = this._formBuilder.group({
+      category: ['', Validators.required],
+      ref_product: ['', Validators.required],
+
     });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required],
+
+    this.inputFormGroup = this._formBuilder.group({
+      input: this._formBuilder.array([]),
     });
-    this.threeFormGroup = this._formBuilder.group({
-      threeCtrl: ['', Validators.required],
+
+    this.donneeFormGroup = this._formBuilder.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      mail: ['', Validators.required],
+      phone: ['', Validators.required]
     });
+  }
+
+  sendEstim(){
+    const body = { title: 'Angular POST Request Example' };
+    this._httpClient.post<Post_lists[]>('http://localhost:3000/price_estim', body).subscribe(data => {
+      this.post_Lists = data;
+      console.log(data)
+    });
+  }
+
+  selectInput() {
+    for (let input of this.inputLists_temp) {
+      if (this.productFormGroup.value.category == input.category) {
+        console.log(input);
+        this.inputLists.push(input);
+        this.addInput(input._id)
+      }
+
+    }
+  }
+
+  selectInputOption() {
+    for (let input of this.inputLists) {
+      if (input.array){
+        for (let input_option of this.input_array_Lists_temp){
+          if ( input._id == input_option.val_func_id) {
+            console.log(input_option);
+            this.input_array_Lists.push(input_option);
+          }
+        }
+      }
+    }
+  }
+
+  submitCat() {
+    if (!this.productFormGroup.valid) {
+      return;
+    }
+    console.log(this.productFormGroup.value.category);
+    this.selectInput()
+    console.log(this.inputLists);
+    console.log(this.inputLists_temp);
+    this.selectInputOption()
+    console.log(this.input_array_Lists);
+    console.log(this.input_array_Lists_temp);
+
+      }
+
+  submitInt(){
+    if (!this.inputFormGroup.valid) {
+      return;
+    }
+    console.log(this.inputFormGroup.value);
+
+  }
+
+  submitDon(){
+    if (!this.donneeFormGroup.valid) {
+      return;
+    }
+    console.log(this.donneeFormGroup.value);
+    this.sendEstim()
+  }
+
+  get input(): FormArray {
+    return this.inputFormGroup.get('input') as FormArray;
+  }
+
+  addInput(_id: string){
+    const inputForm = this._formBuilder.group({
+      id: _id,
+      value: ['', Validators.required]
+      });
+    this.input.push(inputForm)
   }
 
 
