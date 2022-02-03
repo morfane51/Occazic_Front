8b126cc305dfd56473c6d352d59cb896b0e797d6
aymeric <aymeric.baptiste@gmail.com>
2022-02-03 15:36:37 +0100

    Add root category support and optimize call api.

---
+++ src/app/data.service.ts
@@ -0,0 +1,116 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+
+export interface Root_Cat {
+  _id: string;
+  name: string;
+  picture: string;
+}
+
+export interface Cat {
+  _id: string;
+  name: string;
+  marge: number;
+  picture: string;
+  sub_category: [
+    _id: string,
+    name: string,
+    pricture: string
+  ];
+}
+
+export interface Input {
+  _id: string;
+  name: string;
+  array: boolean;
+  category: string;
+}
+
+export interface Input_array_temp {
+  _id: string;
+  name: string;
+  val_func_id: string;
+  value: number;
+}
+
+export interface Input_array {
+  _id: string;
+  name: string;
+  val_func_id: string;
+  value: number;
+}
+
+export interface Estim_lists {
+  _id: string;
+  product_category_id: string;
+}
+
+export interface Input_value_lists {
+  _id: string;
+  product_category_id: string;
+  product_ref: string;
+}
+
+export interface Calcul_lists {
+  _id: string;
+  propose_price: number;
+  marge: number;
+}
+
+@Injectable()
+export class FrontDataService {
+
+  rootCatLists: Root_Cat[] = [];
+  catLists: Cat[] = [];
+  inputLists_temp: Input[] = [];
+  input_array_Lists: Input_array[] = [];
+  input_array_Lists_temp: Input_array_temp[] = [];
+
+  private _rootCatListUrl = 'http://localhost:3000/sub-category';
+  private _catListUrl = 'http://localhost:3000/category';
+  private _inputListUrl = 'http://localhost:3000/val_func';
+  private _input_array_ListUrl = 'http://localhost:3000/array_val';
+  private _estimPostUrl = 'http://localhost:3000/price_estim';
+  private _inputValueUrl = 'http://localhost:3000/input_func';
+  private _calculEstimUrl = 'http://localhost:3000/calcul';
+
+  constructor(public _httpClient: HttpClient) {}
+
+  getRootCategoryData() {
+    return this._httpClient.get<Root_Cat[]>(this._rootCatListUrl)
+  }
+
+  getCategoryData(root_cat_id: string) {
+    return this._httpClient.get<Cat[]>(this._catListUrl + '/root_category/' + root_cat_id)
+  }
+
+  getInputFuncData() {
+    return this._httpClient.get<Input[]>(this._inputListUrl)
+  }
+
+  getArrayFuncData() {
+    return this._httpClient.get<Input_array_temp[]>(this._input_array_ListUrl)
+  }
+
+  postEstimateData(cat_id: string) {
+    const body = {product_category_id: cat_id};
+    return this._httpClient.post<Estim_lists[]>(this._estimPostUrl, body)
+  }
+
+  putEstimateData(name: string, surname: string, mail: string, estimId: string | undefined, calcul?: string | undefined) {
+    const body = { name: name, surname: surname, mail: mail, calcul_id: calcul };
+    return this._httpClient.put<Estim_lists[]>(this._estimPostUrl + '/' + estimId, body)
+  }
+
+  postInputValueData(estimId: string | undefined, id: string, value: string) {
+    const body = {price_estimate_id: estimId, val_func_id: id, value: value};
+    return this._httpClient.post<Input_value_lists[]>(this._inputValueUrl, body)
+  }
+
+  postCalculateData(estimId: string | undefined) {
+    const body = {price_estimID: estimId};
+    return this._httpClient.post<Calcul_lists[]>(this._calculEstimUrl, body)
+  }
+
+}
--- src/app/estimate/estimate.component.ts
+++ src/app/estimate/estimate.component.ts
@@ -4,64 +4,21 @@ import {HttpClient} from '@angular/common/http';
 import {StepperOrientation} from "@angular/cdk/stepper";
 import {BreakpointObserver, BreakpointState} from '@angular/cdk/layout';
 import {MatStepper} from "@angular/material/stepper";
+import {Cat, FrontDataService, Input_array, Input_array_temp, Input, Root_Cat} from "../data.service";
 
-
-interface Cat {
-  _id: string;
-  name: string;
-  marge: number;
-  picture: string;
-}
-
-interface Input_temp {
+export interface Input_temp {
   _id: string;
   name: string;
   array: boolean;
   category: string;
 }
 
-interface Input {
-  _id: string;
-  name: string;
-  array: boolean;
-  category: string;
-}
-
-interface Input_array_temp {
-  _id: string;
-  name: string;
-  val_func_id: string;
-  value: number;
-}
-
-interface Input_array {
-  _id: string;
-  name: string;
-  val_func_id: string;
-  value: number;
-}
-
-interface Estim_lists {
-  _id: string;
-  product_category_id: string;
-}
-
-interface Input_value_lists {
-  _id: string;
-  product_category_id: string;
-  product_ref: string;
-}
-
-interface Calcul_lists {
-  _id: string;
-  propose_price: number;
-  marge: number;
-}
 
 @Component({
   selector: 'app-estimate',
   templateUrl: './estimate.component.html',
-  styleUrls: ['./estimate.component.css']
+  styleUrls: ['./estimate.component.css'],
+  providers: [FrontDataService]
 })
 
 export class EstimateComponent implements OnInit {
@@ -79,48 +36,46 @@ export class EstimateComponent implements OnInit {
   catIsSelect = true;
   cardId: number;
 
+  rootCatSelected: string;
+  rootCatIsSelect = true;
+  rootCatCardId: number;
+
   defaultElevation = 2;
   raisedElevation = 8;
 
-  catLists: Cat[] = [];
+  rootCatLists: Root_Cat[] = [];
+  catLists: Cat[] | undefined = [];
 
   inputLists: Input[] = [];
-  inputLists_temp: Input_temp[] = [];
+  inputLists_temp: Input[] | undefined = [];
 
   input_array_Lists: Input_array[] = [];
-  input_array_Lists_temp: Input_array_temp[] = [];
+  input_array_Lists_temp: Input_array_temp[] | undefined = [];
 
   estim_Lists: any;
   input_Value_Lists: any;
   calcul_Estim_Lists: any;
 
+  rootCategoryId: string | undefined ;
   categoryId: string | undefined ;
   estimId: string | undefined ;
   price_up: number | undefined ;
   price_down: number | undefined ;
 
-  private _catListUrl = 'http://localhost:3000/category';
-  private _inputListUrl = 'http://localhost:3000/val_func';
-  private _input_array_ListUrl = 'http://localhost:3000/array_val';
-  private _estimPostUrl = 'http://localhost:3000/price_estim';
-  private _inputValueUrl = 'http://localhost:3000/input_func';
-  private _calculEstimUrl = 'http://localhost:3000/calcul';
-
   @ViewChild('stepper') stepper: MatStepper;
 
-  constructor(public breakpointObserver: BreakpointObserver, private _formBuilder: FormBuilder, private _httpClient: HttpClient) {
+  constructor(public breakpointObserver: BreakpointObserver, private _formBuilder: FormBuilder, private _httpClient: HttpClient, private dataSrv: FrontDataService) {
   }
 
   ngOnInit() {
-    this._httpClient.get<Cat[]>(this._catListUrl).subscribe(data => {
-      this.catLists = data;
+
+    this.dataSrv.getRootCategoryData().subscribe(data => {
+      this.rootCatLists = data;
     })
-    this._httpClient.get<Input_temp[]>(this._inputListUrl).subscribe(data => {
-      this.inputLists_temp = data;
-    })
-    this._httpClient.get<Input_array_temp[]>(this._input_array_ListUrl).subscribe(data => {
-      this.input_array_Lists_temp = data;
-    })
+
+    this.getInputFuncData();
+
+    this.getArrayFuncData();
 
     this.inputFormGroup = this._formBuilder.group({
       input: this._formBuilder.array([]),
@@ -144,42 +99,104 @@ export class EstimateComponent implements OnInit {
       });
   }
 
+  async getCategoryData(root_cat_id: string) {
+    await this.dataSrv.getCategoryData(root_cat_id).toPromise()
+      .then(data => {
+        this.catLists = data;
+        console.log('Get category data with Root Category ID');
+      })
+      .catch(err => {
+        console.log('Not get category data with Root Category ID error : ' + err);
+      });
+  }
+
+  async getInputFuncData() {
+    await this.dataSrv.getInputFuncData().toPromise()
+      .then(data => {
+        this.inputLists_temp = data;
+        console.log('Get Input Func data with Category ID');
+      })
+      .catch(err => {
+        console.log('Not get Input Func data with Category ID error : ' + err);
+      });
+  }
+
+  async getArrayFuncData() {
+    await this.dataSrv.getArrayFuncData().toPromise()
+      .then(data => {
+        this.input_array_Lists_temp = data;
+        console.log('Get Input Func data with Category ID');
+      })
+      .catch(err => {
+        console.log('Not get Input Func data with Category ID error : ' + err);
+      });
+  }
+
   selectCat(cat_id: string, cardId: number) {
     this.catSelected = cat_id;
     this.catIsSelect = false;
     this.cardId = cardId;
+    console.log(this.catLists)
+  }
+
+  selectRootCat(root_cat_id: string, rootCatCardId: number) {
+    this.rootCatSelected = root_cat_id;
+    this.rootCatIsSelect = false;
+    this.rootCatCardId = rootCatCardId;
+    console.log(this.rootCatLists)
   }
 
   // @ts-ignore
-  focusCardStyle(cardId: number){
+  focusRootCatCardStyle(rootCatCardId: number){
+    const style = "outline: 3px solid #3f51b5;";
+    if(rootCatCardId == this.rootCatCardId){
+      return style;
+    }
+  }
+
+  // @ts-ignore
+  focusCatCardStyle(cardId: number){
     const style = "outline: 3px solid #3f51b5;";
     if(cardId == this.cardId){
       return style;
     }
   }
 
-  validCatSelect(cat_id: string){
+  async validCatSelect(cat_id: string){
     if (cat_id == null) {
       console.log('vide');
       return;
     }
-    // @ts-ignore
-    this.stepper.selected.completed = true;
-    this.stepper.next();
-    console.log(cat_id);
+
     this.categoryId = cat_id;
-    this.selectInput()
+    await this.selectInput()
     console.log(this.inputLists);
-    console.log(this.inputLists_temp);
+
     this.selectInputOption()
     console.log(this.input_array_Lists);
     console.log(this.input_array_Lists_temp);
     this.createEstim(cat_id)
+    // @ts-ignore
+    this.stepper.selected.completed = true;
+    this.stepper.next();
+    console.log(cat_id);
+  }
+
+  async validRootCatSelect(root_cat_id: string){
+    if (root_cat_id == null) {
+      console.log('vide');
+      return;
+    }
+    await this.getCategoryData(root_cat_id)
+    // @ts-ignore
+    this.stepper.selected.completed = true;
+    await this.stepper.next();
+    console.log(root_cat_id);
+    this.rootCategoryId = root_cat_id;
   }
 
   createEstim(cat_id: string) {
-    const body = {product_category_id: cat_id};
-    this._httpClient.post<Estim_lists[]>(this._estimPostUrl, body).subscribe(data => {
+    this.dataSrv.postEstimateData(cat_id).subscribe(data => {
       JSON.stringify(data);
       this.estim_Lists = data;
       this.estimId = this.estim_Lists._id;
@@ -187,26 +204,36 @@ export class EstimateComponent implements OnInit {
 
   }
 
-  editEstim(name: string, surname: string, mail: string, phone: string, calcul?: string) {
-    const body = {name: name, surname: surname, mail: mail, calcul_id: calcul};
-    this._httpClient.put<Estim_lists[]>(this._estimPostUrl + '/' + this.estimId, body).subscribe(data => {
-      this.estim_Lists = data;
-      console.log(data)
-    });
+  async editEstim(name: string, surname: string, mail: string, phone: string, calcul?: string) {
+    let estimId = await this.estimId
+    await this.dataSrv.putEstimateData(name, surname, mail, estimId, calcul)
+      .toPromise()
+      .then(data => {
+        this.estim_Lists = data;
+        console.log(data)
+      })
+      .catch(err => {
+        console.log('Not edit estimate error : ' + err);
+      });
   }
 
-  sendInputValue(id: string, value: string) {
-    const body = {price_estimate_id: this.estimId, val_func_id: id, value: value};
-    this._httpClient.post<Input_value_lists[]>(this._inputValueUrl, body).subscribe(data => {
-      JSON.stringify(data);
-      this.input_Value_Lists = data;
-      console.log(data)
-    });
+  async sendInputValue(id: string, value: string) {
+    let estimId = await this.estimId
+    await this.dataSrv.postInputValueData(estimId, id, value)
+      .toPromise()
+      .then(data => {
+        JSON.stringify(data);
+        this.input_Value_Lists = data;
+        console.log(data);
+      })
+      .catch(err => {
+        console.log('Not send input value error : ' + err);
+      });
   }
 
   calculEstim(){
-    const body = {price_estimID: this.estimId};
-    this._httpClient.post<Calcul_lists[]>(this._calculEstimUrl, body).subscribe(data => {
+    let estimId = this.estimId
+    this.dataSrv.postCalculateData(estimId).subscribe(data => {
       JSON.stringify(data);
       this.calcul_Estim_Lists = data;
       console.log(data)
@@ -245,7 +272,9 @@ export class EstimateComponent implements OnInit {
     this.calculEstim();
   }
 
+
   selectInput() {
+    // @ts-ignore
     for (let input of this.inputLists_temp) {
       if (this.categoryId == input.category) {
         console.log(input);
@@ -259,6 +288,7 @@ export class EstimateComponent implements OnInit {
   selectInputOption() {
     for (let input of this.inputLists) {
       if (input.array) {
+        // @ts-ignore
         for (let input_option of this.input_array_Lists_temp) {
           if (input._id == input_option.val_func_id) {
             console.log(input_option);
